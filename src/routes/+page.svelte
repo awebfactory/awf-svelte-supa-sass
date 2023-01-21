02d7d2f91d5324f071a2b11a5e6e8ffaee6c1871
Victor Kane <victorkane@gmail.com>
2023-01-21 10:28:35 -0300

    build: Add public static environment variables and temporarily test render on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
 <h1>SAAS with SvelteKit & Supabase</h1>
 <p>A demo Software as a Service solution, using SvelteKit & Supabase</p>
+
+<p>Supabase REST API URL: {data.supabaseUrl}</p>
+<p>Supabase ANON KEY: {data.supabaseAnonKey}</p>
